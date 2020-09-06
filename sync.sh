12b5f62c23db6877f5d86346afb493215fe516b0
percy <xhc_1007@foxmail.com>
2020-09-06 10:42:48 +0800

    updated at 2020-09-06 10:42:48

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 # synchronize the generated file
 rm -r 404.html about assets feed.xml index.html jekyll js
-cp -r ~/myBlog/_site/* .
+cp -r ~/workSpace/myBlog/_site/* .
 
 git add .
 git commit -m "updated at `date '+%Y-%m-%d %H:%M:%S'`"
